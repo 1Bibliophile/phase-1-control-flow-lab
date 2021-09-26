add5bcc670bb422d7fc5c437d6d3b100f3ca64d5
David Morgenstern <DavidtheConfident@gmail.com>
2021-09-26 17:42:03 -0500

    commit tree 115faebc837367f7a11204f5d0be74112e70fb95

--- index.js
+++ index.js
@@ -1,11 +1,60 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(num){
   // Write your code here!
+  if(num < 400 || num === 400)
+  {
+      return 'This one is on me!'
+  }
+  else if(num > 2000)
+  {
+      return 'I will gladly take your thirty bucks.'
+  }
+  else if(num > 2500)
+  {
+      return 'I will gladly take your thirty bucks.'
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  
+  if(city === 'NYC')
+  {
+      return "Ok, sounds good."
+  }
+  else 
+  {
+      return "No go."
+  }
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+ /* if(tip === 'generous')
+  {
+      return 'Thank you so much.'
+  }
+  else if(tip === 'not as generous')
+  {
+      return "Thank you."
+  }
+  else if(tip === 'thanks for everything')
+  {
+      return 'Bye.'
+  }
+*/
+  switch(tip){
+    case 'generous': 
+        tip = 'Thank you so much.'
+        break;
+    case 'not as generous': 
+        tip = 'Thank you.'
+        break;
+    case 'thanks for everything': 
+        tip = 'Bye.'
+        break;
+    
+
+  }
+  return tip;
 }
+
